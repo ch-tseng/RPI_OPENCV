d4065c6923605cc962be37ab53ff04963652bd33
ch-tseng <ch.tseng@sunplusit.com>
2017-05-30 19:05:59 +0800

    update

--- motionDetect.py
+++ motionDetect.py
@@ -10,6 +10,8 @@ from libraryCH.device.lcd import ILI9341
 
 lcd = ILI9341(LCD_size_w=240, LCD_size_h=320, LCD_Rotate=270)
 
+lcdDislpay = 2  #1--> Contour  2--> Image
+
 numInput = raw_input("Please enter your gesture number: ")
 
 def wait():
@@ -43,24 +45,30 @@ while(True):
     blur2 = cv2.GaussianBlur(grey2,(5,5),0)
 
     d=cv2.absdiff(blur1,blur2)
-    ret, th = cv2.threshold( d, 15, 255, cv2.THRESH_BINARY )
+    ret, th = cv2.threshold( d, 10, 255, cv2.THRESH_BINARY )
     dilated=cv2.dilate(th,k,iterations=2)
     #canny = cv2.Canny(dilated, 50, 150) 
     contours, hierarchy = cv2.findContours(dilated,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     areas = [cv2.contourArea(c) for c in contours]
-    t2 = t0
+
+    if(lcdDislpay==1):
+        layer = np.zeros(t0.shape[:2], dtype = "uint8")
+    elif(lcdDislpay==2):
+        layer = t0
+
     if(len(areas)>0):
         max_index = np.argmax(areas)
         cnt=contours[max_index]
         x,y,w,h = cv2.boundingRect(cnt)
         print("area={}, w*h={}".format(areas[max_index], w*h))
-        if(areas[max_index]>3000):
-            cv2.rectangle(t2,(x,y),(x+w,y+h),(0,255,0),2)
-    #t2=t0
-    #cv2.drawContours(t2, contours, -1, (0,255,0), 2 )
-    lcd.displayImg(t2)
-    if(not numInput==""): writeImage(i, canny)
+        if(areas[max_index]>15000):
+            cv2.drawContours(layer, cnt, -1, 255, 2)
+            cv2.rectangle(layer,(x,y),(x+w,y+h),255 ,2)
+
+    lcd.displayImg(layer)
+
+    if(not numInput==""): writeImage(i, layer)
     #print("dilated.shape={}".format(dilated.shape))
 
     t0=t1
